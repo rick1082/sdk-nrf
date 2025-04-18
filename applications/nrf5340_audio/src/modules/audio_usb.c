7a2e26d6ff95b18c7b66cafc9522cb0641b199a1
Jui-Chou Chung <jui-chou.chung@nordicsemi.no>
2025-04-18 15:17:46 +0800

    Add USB HID interface

--- applications/nrf5340_audio/src/modules/audio_usb.c
+++ applications/nrf5340_audio/src/modules/audio_usb.c
@@ -296,15 +296,208 @@ int audio_usb_send_key(uint8_t key)
 	return 0;
 }
 
+#include <sample_usbd.h>
+#include <zephyr/usb/usbd.h>
+#include <zephyr/usb/class/usbd_hid.h>
+static const uint8_t hid_report_desc_keyboard[] = HID_KEYBOARD_REPORT_DESC();
+
+enum kb_leds_idx {
+	KB_LED_NUMLOCK = 0,
+	KB_LED_CAPSLOCK,
+	KB_LED_SCROLLLOCK,
+	KB_LED_COUNT,
+};
+
+static const struct gpio_dt_spec kb_leds[KB_LED_COUNT] = {
+	GPIO_DT_SPEC_GET_OR(DT_ALIAS(led0), gpios, {0}),
+	GPIO_DT_SPEC_GET_OR(DT_ALIAS(led1), gpios, {0}),
+	GPIO_DT_SPEC_GET_OR(DT_ALIAS(led2), gpios, {0}),
+};
+
+enum kb_report_idx {
+	KB_MOD_KEY = 0,
+	KB_RESERVED,
+	KB_KEY_CODE1,
+	KB_KEY_CODE2,
+	KB_KEY_CODE3,
+	KB_KEY_CODE4,
+	KB_KEY_CODE5,
+	KB_KEY_CODE6,
+	KB_REPORT_COUNT,
+};
+
+struct kb_event {
+	uint16_t code;
+	int32_t value;
+};
+
+K_MSGQ_DEFINE(kb_msgq, sizeof(struct kb_event), 2, 1);
+
+UDC_STATIC_BUF_DEFINE(report, KB_REPORT_COUNT);
+static uint32_t kb_duration;
+static bool kb_ready;
+
+static void kb_iface_ready(const struct device *dev, const bool ready)
+{
+	LOG_INF("HID device %s interface is %s",
+		dev->name, ready ? "ready" : "not ready");
+	kb_ready = ready;
+}
+
+static int kb_get_report(const struct device *dev,
+			 const uint8_t type, const uint8_t id, const uint16_t len,
+			 uint8_t *const buf)
+{
+	LOG_WRN("Get Report not implemented, Type %u ID %u", type, id);
+
+	return 0;
+}
+
+static int kb_set_report(const struct device *dev,
+			 const uint8_t type, const uint8_t id, const uint16_t len,
+			 const uint8_t *const buf)
+{
+	if (type != HID_REPORT_TYPE_OUTPUT) {
+		LOG_WRN("Unsupported report type");
+		return -ENOTSUP;
+	}
+/*
+	for (unsigned int i = 0; i < ARRAY_SIZE(kb_leds); i++) {
+		if (kb_leds[i].port == NULL) {
+			continue;
+		}
+
+		(void)gpio_pin_set_dt(&kb_leds[i], buf[0] & BIT(i));
+	}
+*/
+	return 0;
+}
+
+/* Idle duration is stored but not used to calculate idle reports. */
+static void kb_set_idle(const struct device *dev,
+			const uint8_t id, const uint32_t duration)
+{
+	LOG_INF("Set Idle %u to %u", id, duration);
+	kb_duration = duration;
+}
+
+static uint32_t kb_get_idle(const struct device *dev, const uint8_t id)
+{
+	LOG_INF("Get Idle %u to %u", id, kb_duration);
+	return kb_duration;
+}
+
+static void kb_set_protocol(const struct device *dev, const uint8_t proto)
+{
+	LOG_INF("Protocol changed to %s",
+		proto == 0U ? "Boot Protocol" : "Report Protocol");
+}
+
+static void kb_output_report(const struct device *dev, const uint16_t len,
+			     const uint8_t *const buf)
+{
+	LOG_HEXDUMP_DBG(buf, len, "o.r.");
+	kb_set_report(dev, HID_REPORT_TYPE_OUTPUT, 0U, len, buf);
+}
+
+struct hid_device_ops kb_ops = {
+	.iface_ready = kb_iface_ready,
+	.get_report = kb_get_report,
+	.set_report = kb_set_report,
+	.set_idle = kb_set_idle,
+	.get_idle = kb_get_idle,
+	.set_protocol = kb_set_protocol,
+	.output_report = kb_output_report,
+};
+
+/* doc device msg-cb start */
+static void msg_cb(struct usbd_context *const usbd_ctx,
+		   const struct usbd_msg *const msg)
+{
+	LOG_INF("USBD message: %s", usbd_msg_type_string(msg->type));
+
+	if (msg->type == USBD_MSG_CONFIGURATION) {
+		LOG_INF("\tConfiguration value %d", msg->status);
+	}
+
+	if (usbd_can_detect_vbus(usbd_ctx)) {
+		if (msg->type == USBD_MSG_VBUS_READY) {
+			if (usbd_enable(usbd_ctx)) {
+				LOG_ERR("Failed to enable device support");
+			}
+		}
+
+		if (msg->type == USBD_MSG_VBUS_REMOVED) {
+			if (usbd_disable(usbd_ctx)) {
+				LOG_ERR("Failed to disable device support");
+			}
+		}
+	}
+}
+static K_SEM_DEFINE(ep_write_sem, 0, 1);
+static void int_in_ready_cb(const struct device *dev)
+{
+	ARG_UNUSED(dev);
+	k_sem_give(&ep_write_sem);
+}
+#include <zephyr/usb/usbd.h>
+#include <zephyr/usb/class/usb_hid.h>
+static const struct hid_ops mouse_ops = {
+	.int_in_ready = int_in_ready_cb,
+};
+#define MOUSE_BTN_LEFT		0
+#define MOUSE_BTN_RIGHT		1
+
+enum mouse_report_idx {
+	MOUSE_BTN_REPORT_IDX = 0,
+	MOUSE_X_REPORT_IDX = 1,
+	MOUSE_Y_REPORT_IDX = 2,
+	MOUSE_WHEEL_REPORT_IDX = 3,
+	MOUSE_REPORT_COUNT = 4,
+};
+K_MSGQ_DEFINE(mouse_msgq, MOUSE_REPORT_COUNT, 2, 1);
+
+/* doc device msg-cb end */
+static const uint8_t hid_report_desc_mouse[] = HID_MOUSE_REPORT_DESC(2);
+
 int audio_usb_init(void)
 {
 	int ret;
 	LOG_INF("USB audio init");
 
-	const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(uac2_headset));
+	const struct device *headset = DEVICE_DT_GET(DT_NODELABEL(uac2_headset));
+	const struct device *hid_dev_keyboard;
+	const struct device *hid_dev_mouse;
 	struct usbd_context *sample_usbd;
 
-	usbd_uac2_set_ops(dev, &usb_audio_ops, &main_ctx);
+	usbd_uac2_set_ops(headset, &usb_audio_ops, &main_ctx);
+
+	hid_dev_keyboard = DEVICE_DT_GET(DT_NODELABEL(hid_dev_0));
+	if (!device_is_ready(hid_dev_keyboard)) {
+		LOG_ERR("HID Device is not ready");
+		return -EIO;
+	}
+
+	ret = hid_device_register(hid_dev_keyboard,
+		hid_report_desc_keyboard, sizeof(hid_report_desc_keyboard),
+				  &kb_ops);
+	if (ret != 0) {
+		LOG_ERR("Failed to register HID Device, %d", ret);
+		return ret;
+	}
+
+	hid_dev_mouse = DEVICE_DT_GET(DT_NODELABEL(hid_dev_1));
+	if (!device_is_ready(hid_dev_mouse)) {
+		LOG_ERR("HID Device is not ready");
+		return -EIO;
+	}
+
+	usb_hid_register_device(hid_dev_mouse,
+		hid_report_desc_mouse, sizeof(hid_report_desc_mouse),
+		&mouse_ops);
+
+	usb_hid_init(hid_dev_mouse);
+
 
 	sample_usbd = sample_usbd_init_device(NULL);
 	if (sample_usbd == NULL) {
