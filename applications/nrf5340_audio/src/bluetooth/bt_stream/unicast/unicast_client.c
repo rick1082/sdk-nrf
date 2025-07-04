2854fc44e11b21444b2357ada778956fca5cb9a9
Jui-Chou Chung <jui-chou.chung@nordicsemi.no>
2025-07-04 17:02:46 +0800

    fix the wrong preset sampling rate from client side

--- applications/nrf5340_audio/src/bluetooth/bt_stream/unicast/unicast_client.c
+++ applications/nrf5340_audio/src/bluetooth/bt_stream/unicast/unicast_client.c
@@ -93,7 +93,7 @@ static struct bt_bap_unicast_group *unicast_group;
 static bool unicast_group_created;
 
 #define BT_BAP_LC3_UNICAST_PRESET_STEREO_16_2_1(_loc, _stream_context)                                    \
-	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_24KHZ,                 \
+	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_16KHZ,                 \
 						    BT_AUDIO_CODEC_CFG_DURATION_10, _loc, 40U, 1,  \
 						    _stream_context),                              \
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(10000u, 80u, 2u, 10u, 40000u))
@@ -105,7 +105,7 @@ static bool unicast_group_created;
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(10000u, 120u, 2u, 10u, 40000u))
 
 #define BT_BAP_LC3_UNICAST_PRESET_STEREO_48_2_1(_loc, _stream_context)                                    \
-	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_24KHZ,                 \
+	BT_BAP_LC3_PRESET(BT_AUDIO_CODEC_LC3_CONFIG(BT_AUDIO_CODEC_CFG_FREQ_48KHZ,                 \
 						    BT_AUDIO_CODEC_CFG_DURATION_10, _loc, 120U, 1,  \
 						    _stream_context),                              \
 			  BT_AUDIO_CODEC_QOS_UNFRAMED(10000u, 240u, 2u, 10u, 40000u))
